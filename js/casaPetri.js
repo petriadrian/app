78cf1e8e7da2eca615a71872aed70d651bd8e54b
Adrian Petri <Adrian.Petri@betfair.com>
2015-07-22 21:13:56 +0300

    try to rezolve import

--- js/casaPetri.js
+++ js/casaPetri.js
@@ -6,20 +6,23 @@ $('.nav a').on('click', function(){
    $(".navbar-toggle").click()
 });
 
-function ContentCtrl($scope, $http) {
-    "use strict";
+var app = angular.module('plunker', []);
 
-    $scope.url = '../pages/commonStuffs/content/reservation.json';
-    $scope.content = [];
-
-    $scope.fetchContent = function() {
-        $http.get($scope.url).then(function(result){
-            $scope.content = result.data;
-        });
-    }
-
-    $scope.fetchContent();
-}
+app.controller('MainCtrl', function($scope) {
+    $scope.menuBox = [{
+        "title": "MERGE",
+        "hoverTitle": "Rezerva o camera",
+        "homePageRef": "pages/accommodation/page.html",
+        "otherPageRef": "../../accommodation/page.html",
+        "homePageFirstImageRef": "images/accomodation/rooms/1.png",
+        "homePageSecondImageRef": "../../images/accomodation/rooms/1.png",
+        "homePageFirstImageRef": "images/accomodation/rooms/2.png",
+        "homePageSecondImageRef": "../../images/accomodation/rooms/2.png",
+        "homePageFirstImageRef": "images/accomodation/rooms/3.png",
+        "homePageSecondImageRef": "../../images/accomodation/rooms/3.png",
+        "boxTitle": "Casu?a a fost cinematograf, spital ?i banc? iar acum este redeschis? ca spa?iu de cazare. Vizita?i-o!",
+    }];
+});
 
 // Unuused functions
 // Add active when a button clicked
