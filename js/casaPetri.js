14715feefb7924598ca8460ed8eeb2d4bb49134d
petriadrian <petriadrian@gmail.com>
2015-08-21 00:21:37 +0300

    album gallery

--- js/casaPetri.js
+++ js/casaPetri.js
@@ -6,6 +6,20 @@ $('.nav a').on('click', function(){
    $(".navbar-toggle").click()
 });
 
+// photo album slider config
+$(".fancybox").fancybox({
+    helpers : {
+        title: {
+            type: 'inside',
+            position: 'bottom'
+        },
+        overlay : {
+            css : {
+                'background' : 'rgba(0, 0, 0, 0.85)'
+            }
+        }
+    }
+});
 // Unused functions
 // Add active when a button clicked
 //    $(document).ready(function () {
