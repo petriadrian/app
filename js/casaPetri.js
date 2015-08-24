fcaf7a518941ca1df578bcd4495131cc4459210d
petriadrian <petriadrian@gmail.com>
2015-08-25 00:29:26 +0300

    fix anchor

--- js/casaPetri.js
+++ js/casaPetri.js
@@ -2,6 +2,13 @@ $('.nav a').on('click', function(){
    $(".navbar-toggle").click()
 });
 
+function getParameterFromUrl(name) {
+   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+       results = regex.exec(location.search);
+   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 // Unused functions
 // Add active when a button clicked
 //    $(document).ready(function () {
