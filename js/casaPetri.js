9aaba04e4fbec52b66e55c223da56090403740e4
Adrian Petri <Adrian.Petri@betfair.com>
2015-10-27 18:28:50 +0200

    trasee

--- js/casaPetri.js
+++ js/casaPetri.js
@@ -24,6 +24,25 @@ function dynamicContentLoader(templatePathSuffix, contentPathSuffix, idParentEle
     });
 }
 
+function loadContentWithSpecificIds(templatePathSuffix, contentPathSuffix, idsOfTheContentToBeLoadedPathSuffix, idParentElement, templateId) {
+    $.getJSON('/casaPetri/content/' + getLanguage() + '/' + idsOfTheContentToBeLoadedPathSuffix, function (specificItemsIds) {
+        var neededItemsIds = [];
+        $.each(specificItemsIds, function (key, item) {
+            neededItemsIds.push(item.id);
+        });
+        $.getJSON('/casaPetri/content/' + getLanguage() + '/' + contentPathSuffix, function (allItems) {
+            $.get('/casaPetri/template/' + templatePathSuffix, function (template) {
+                $('#' + idParentElement).append(template);
+                $.each(allItems, function (key, item) {
+                    if (neededItemsIds.indexOf(item.id) > -1) {
+                        $('#' + templateId).tmpl(item).appendTo('#' + idParentElement);
+                    }
+                });
+            });
+        });
+    });
+}
+
 function htmlContentLoader(htmlPageSuffix, idParentElement) {
     $.get('/casaPetri/pages/' + htmlPageSuffix, function (content) {
         $('#' + idParentElement).append(content);
@@ -44,9 +63,9 @@ function changeLanguageTo(newLang) {
     if (newLang == EN_LOCALE) {
         alert("We are sorry! The english version of this site is not ready yet.");
     }
-        var currentUrl = window.location.href;
-        var redirectUrl = replaceUrlParam(currentUrl, LANG_URL_PARAM, newLang);
-        window.location.replace(redirectUrl);
+    var currentUrl = window.location.href;
+    var redirectUrl = replaceUrlParam(currentUrl, LANG_URL_PARAM, newLang);
+    window.location.replace(redirectUrl);
 
 }
 
@@ -68,24 +87,22 @@ function getLanguage() {
 function getURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
+    for (var i = 0; i < sURLVariables.length; i++) {
         var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam)
-        {
+        if (sParameterName[0] == sParam) {
             return sParameterName[1];
         }
     }
 }
 
-function replaceUrlParam(url, paramName, paramValue){
-    var pattern = new RegExp('('+paramName+'=).*?(&|$)')
-    var newUrl=url
-    if(url.search(pattern)>=0){
-        newUrl = url.replace(pattern,'$1' + paramValue + '$2');
+function replaceUrlParam(url, paramName, paramValue) {
+    var pattern = new RegExp('(' + paramName + '=).*?(&|$)')
+    var newUrl = url
+    if (url.search(pattern) >= 0) {
+        newUrl = url.replace(pattern, '$1' + paramValue + '$2');
     }
-    else{
-        newUrl = newUrl + (newUrl.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue
+    else {
+        newUrl = newUrl + (newUrl.indexOf('?') > 0 ? '&' : '?') + paramName + '=' + paramValue
     }
     return newUrl
 }
