10732176856cbfd5cb61e617a004edc96b2f7479
petriadrian <petriadrian@gmail.com>
2015-11-24 09:04:34 +0200

    angular

---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+var bookingApp = angular.module('bookingApp', []);
+
+bookingApp.controller('LoadPageDataCtrl', function ($scope, $http) {
+    $http.get('/casapetri/content/ro/activities/trails/letySulei/pageStructure.json').success(function(data){
+        $scope.page = data[0];
+    });
+});
